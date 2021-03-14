eb3913090ca1e02aaae030148d555d6ec0af4f5e
irpanch <irpanch@gmail.com>
2021-03-14 15:28:49 +0700

    fixing text2

--- R_Script.R
+++ R_Script.R
@@ -19,18 +19,18 @@ data_filter <- subset(data_hujan,data_hujan[,2] >= 0.1)
 library(extremeStat) 
 # Untuk data hujan
 dlf_P <- distLfit(data_filter$P)
-plotLfit(dlf_P,legend = T,col = "grey", main = "Grafik PDF Hujan Jam-jaman", xlab="Hujan (mm)",nbest = 6)
+plotLfit(dlf_P,legend = T,col = "cadetblue1", main = "Grafik PDF Hujan Jam-jaman", xlab="Hujan (mm)",nbest = 6)
 plotLfit(dlf_P, cdf=TRUE, main = "Grafik CDF Hujan Jam-jaman", xlab="Hujan (mm)")
 
 # Untuk data debit
 dlf_Debit <- distLfit(data_debit_eto$Debit)
-plotLfit(dlf_Debit,legend = T,col = "grey", main = "Grafik PDF Debit Harian", xlab="Debit (m^3/dtk)",nbest = 6)
+plotLfit(dlf_Debit,legend = T,col = "cornflowerblue", main = "Grafik PDF Debit Harian", xlab="Debit (m^3/dtk)",nbest = 6)
 plotLfit(dlf_Debit, cdf=TRUE, main = "Grafik CDF Debit Harian", xlab="Debit (m^3/dtk)")
 
 # Untuk data eto
 dlf_Eto <- distLfit(data_debit_eto$Eto)
-plotLfit(dlf_Eto,legend = T,col = "grey", main = "Grafik PDF Evapotranspirasi Harian", xlab="Eto (mm)",nbest = 6)
-plotLfit(dlf_Eto, cdf=TRUE, main = "Grafik CDF Evapotranspirasi Harian", xlab="Eto (mm)")
+plotLfit(dlf_Eto,legend = T,col = "aquamarine", main = "Grafik PDF Evapotranspirasi Potensial Harian", xlab="Eto (mm)",nbest = 6)
+plotLfit(dlf_Eto, cdf=TRUE, main = "Grafik CDF Evapotranspirasi Potensial Harian", xlab="Eto (mm)")
 
 
 
